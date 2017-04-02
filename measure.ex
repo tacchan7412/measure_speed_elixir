8e0a4053d5f8c6de737425606894dac8b6eee09f
tacchan7412 <tacchan.04@gmail.com>
2017-04-02 23:46:07 +0900

    first commit

---
+++ measure.ex
@@ -0,0 +1,41 @@
+defmodule Measure do
+
+  def start do
+    str = "aghfiahfoishgrfniorsahjnaiioahfuahilhfuazhiahuhfahfkahfahjkfafkjgsakghakujhfjkaghkfhlaskfjcnjrjkfvbsuirjhbfvjbnsjkfvnjfksdhgvjkfhsjkvhfjkshgvjfkshvjkbfshvfsdgvjkfshvshjlkvjksfdv.lfsdvkhkflsdvhlkfsdhvnkfsnvksdhfvkjlshdkjdhsghjsikrghioasjklfasjhjafgiohgiuhraiohgiotsjhiake.hgnsghiorsjfpr4iosegoirhniseojfaioreoswghotrjhbidgjoptdjhgibotdjrm;olgjiotdjgojsiohtgiosehgiuhisorjrtgfiorsyiuhbngisjfiorysifgjresiolkhasklfhklshfhd"
+    word = "a"
+    count(:regex, str, word)
+    count(:string_split, str, word)
+  end
+
+  def count(:regex, str, word) do
+    IO.inspect "Regex"
+    IO.inspect "-------------"
+    # 指定サイズのDict(Map or HashDict)を作成する無名関数
+    f = fn ->
+      {_, r} = Regex.compile(word)
+      Regex.scan(r, str) |> length
+    end
+    # 無名関数を格納した変数をtimer.tcに渡す
+    {time, count} = :timer.tc(f)
+    IO.inspect "count: #{count}"
+    IO.inspect "time: #{time} μs"
+    IO.inspect "-------------"
+  end
+
+  def count(:string_split, str, word) do
+    IO.inspect "String.split"
+    IO.inspect "-------------"
+    # 指定サイズのDict(Map or HashDict)を作成する無名関数
+    f = fn ->
+      count = String.split(str, word) |> length
+      count - 1
+    end
+    # 無名関数を格納した変数をtimer.tcに渡す
+    {time, count} = :timer.tc(f)
+    IO.inspect "count: #{count}"
+    IO.inspect "time: #{time} μs"
+    IO.inspect "-------------"
+  end
+end
+
+Measure.start
